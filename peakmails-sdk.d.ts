4865dfe6d4b5a3ab306d883538a8edf5111eb174
abyshake-anand <abishakeanand+git@gmail.com>
2024-08-11 20:27:32 +0530

    initialising peakmails sdk

---
+++ peakmails-sdk.d.ts
@@ -0,0 +1,40 @@
+declare module 'peakmails-sdk' {
+    export interface PeakmailsSDKOptions {
+        apiKey: string;
+        domain: string;
+        projectId: string;
+        secretKey?: string;
+        baseUrl?: string;
+    }
+
+    export interface ContactData {
+        email: string;
+        name?: string;
+        customFields?: Record<string, any>;
+    }
+
+    export interface CategoryAssignment {
+        email: string;
+        categories: string[];
+    }
+
+    export interface ScenarioTrigger {
+        scenario: string;
+        email: string;
+    }
+
+    export interface ScenarioCustomFieldsRequest {
+        scenario: string;
+    }
+
+    export class PeakmailsSDK {
+        constructor(options: PeakmailsSDKOptions);
+
+        addContact(contactData: ContactData): Promise<any>;
+        addContactToCategories(assignment: CategoryAssignment): Promise<any>;
+        triggerScenario(trigger: ScenarioTrigger): Promise<any>;
+        getScenarioCustomFields(request: ScenarioCustomFieldsRequest): Promise<any>;
+    }
+
+    export default PeakmailsSDK;
+}
